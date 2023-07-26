ec079d934a2fa04b99bf3ccbdcd34e40029150d3
Wesley Moraes <wesleycrz@hotmail.com>
2023-07-26 00:21:45 -0300

    componentizacao de forms finalizado

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,16 @@
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
+
 function NewProject(){
     return(
-        <h1>NewProject</h1>
+        <div className={styles.newproject_conainer}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar seus serviços</p>
+
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
     )
 }
 
