e928ac52ed00170eec29865fc7b8ddf82476333f
Wesley Moraes <wesleycrz@hotmail.com>
2023-07-27 15:36:25 -0300

    API inicializada

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
@@ -5,6 +7,22 @@ import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=> {
+        fetch("http://localhost:5000/categories",{
+        method: "GET",
+        headers: {
+            "Content-Type" : "application/json"
+        }
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return(
         <form className={styles.form}>
             <Input 
@@ -22,7 +40,7 @@ function ProjectForm({btnText}){
             <Select
                 name="category_id"
                 text="selecione a categoria"
-
+                options = {categories}
             
             />
             <SubmitButton text={btnText}/>
