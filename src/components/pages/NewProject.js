664aef1b273d4da6e42b21756d73a3f52192d7b1
Wesley Moraes <wesleycrz@hotmail.com>
2023-08-14 15:52:17 -0300

    up

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,46 @@
+import { useNavigate } from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm'
 
 import styles from './NewProject.module.css'
 
 
 function NewProject(){
+
+    const history = useNavigate()
+
+    function createPost(project){
+
+        //Initialize cost and services
+        project.cost = 0
+        project.services = []
+        //Ao longo da utilização o custo vai aumentando ou diminuindo, inclusive na questão dos serviços. 
+        //Mas a principio temos que iniciar com o valor 0 ou vazio
+
+        fetch("http://localhost:5000/projects",{
+            method : "POST",
+            headers : {
+                "Content-type" : "application/json"
+
+            },
+            body : JSON.stringify(project),
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            //redirect
+            history('/projects', {
+                message: 'Projeto criado com sucesso!'
+            })
+        })
+        .catch((err) => console.log(err))
+
+    }
+
     return(
         <div className={styles.newproject_conainer}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar seus serviços</p>
 
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit = {createPost} btnText="Criar Projeto"/>
         </div>
     )
 }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -6,9 +6,10 @@ import SubmitButton from '../form/SubmitButton'
 
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}){
+function ProjectForm({handleSubmit, btnText, projectData}){
 
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {}) //Para edição! Ou eu crio algo novo ou já tenho e quero editar
 
     useEffect(()=> {
         fetch("http://localhost:5000/categories",{
@@ -23,24 +24,49 @@ function ProjectForm({btnText}){
         .catch((err) => console.log(err))
     }, [])
 
+    const submit = (e) => {
+        e.preventDefault()
+        //console.log(project)
+        handleSubmit(project) //Executo o método que foi enviado pela props, e passo o projeto cadastrado no formulário
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name] : e.target.value})
+        //console.log(project)
+    } //Método reutilizavel em qualquer input que eu tenha! Independente do input que eu tenha, eu executo ele
+
+    function handleCategory(e){
+        setProject({...project, category:{
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,   
+            }
+        })
+        //console.log(project)
+    }
     return(
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
             <Input 
                 type="text" 
                 text="Nome do projeto" 
                 name="name" 
                 placeholder="Insira o nome do projeto"
+                handleOnChange={handleChange}
+                value={project.name ? project.name : ''}
             />
             <Input
                 type="number"
                 text="Orçamento do projeto"
                 name="budget"
                 placeholder="Insira o orçamento total"
+                handleOnChange={handleChange}
+                value={project.budget ? project.budget : ''}
             />
             <Select
                 name="category_id"
                 text="selecione a categoria"
                 options = {categories}
+                handleOnChange={handleCategory}
+                value={project.category ? project.category.id : ''}
             
             />
             <SubmitButton text={btnText}/>
