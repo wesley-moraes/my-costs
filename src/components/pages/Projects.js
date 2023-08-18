efb21e8659757da0a0537d13696c28ad828828a8
Wesley Moraes <wesleycrz@hotmail.com>
2023-08-18 15:45:24 -0300

    project card

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,16 @@
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+
 import { useLocation } from "react-router-dom"
 
+import styles from './Projects.module.css'
+import LinkButton from "../layout/LInkButton"
+import ProjectCard from "../project/ProjectCard"
+import { useState, useEffect } from "react"
+
 function Projects(){
+
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
@@ -9,14 +18,45 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json'
+
+            },
+
+        }).then(resp => resp.json())
+        .then(data => {
+            setProjects(data);
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
     return(
-        <div>
-            <h1>Meus projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
+            
             {message && 
                 <Message type="success" msg={message} />
             }
-            
-            
+            <Container customClass="start">
+                {projects.length > 0 &&
+                    projects.map(
+                        (project) => 
+                            <ProjectCard 
+                                id = { project.id }
+                                name = { project.name } 
+                                budget = { project.budget }
+                                category = {project?.category?.name}
+                                key = { project.id }
+                            />
+                        )}
+            </Container>
         </div>
     )
 }
